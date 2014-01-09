a7e9d5de2036a1a2222206ecbadfda4e8dfde33a
Abey Mathew <amathew@lat-amathew.careerbuilder.com>
2014-01-09 14:49:41 -0500

    Finish layout and routes

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe StaticPagesController do
 
-  describe "GET '...'" do
-    it "returns http success" do
-      get '...'
-      response.should be_success
-    end
-  end
+#  describe "GET 'Ruby on Rails'" do
+#    it "returns http success" do
+#      get 'Ruby on Rails'
+#      response.should be_success
+#    end
+#  end
 
 end
