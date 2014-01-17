10dd3cd07ee0458d67459c82baf21770c22d4710
Abey Mathew <amathew@lat-amathew.careerbuilder.com>
2014-01-17 14:47:20 -0500

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,37 +1,36 @@
-require 'spec_helper'
+	require 'spec_helper'
 
-describe User do
+	describe User do
 
-	before do
-		@user = User.new(name: "Example User", email: "user@example.com",
-			password: "foobar", password_confirmation: "foobar")
-	end
+		before do
+			@user = User.new(name: "Example User", email: "user@example.com",
+				password: "foobar", password_confirmation: "foobar")
+		end
 
-	subject { @user }
+		subject { @user }
 
-	it { should respond_to(:name) }
-	it { should respond_to(:email) }
-	it { should respond_to(:password_digest) }
-	it { should respond_to(:password) }
-	it { should respond_to(:password_confirmation) }
-	it { should respond_to(:authenticate) }
+		it { should respond_to(:name) }
+		it { should respond_to(:email) }
+		it { should respond_to(:password_digest) }
+		it { should respond_to(:password) }
+		it { should respond_to(:password_confirmation) }
+		it { should respond_to(:authenticate) }
 
-	it { should be_valid }
+		it { should be_valid }
 
-	describe "when name is not present" do
-		before { @user.name = " " }
-		it { should_not be_valid }
-	end
+		describe "when name is not present" do
+			before { @user.name = " " }
+			it { should_not be_valid }
+		end
 
-	describe "when name is too long" do
-		before { @user.name = "a" * 51 }
-		it { should_not be_valid }
-	end
+		describe "when name is too long" do
+			before { @user.name = "a" * 51 }
+			it { should_not be_valid }
+		end
 
-	describe "when email format is invalid" do
-		it "should be invalid" do
-			addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-				foo@bar_baz.com foo@bar+baz.com]
+		describe "when email format is invalid" do
+			it "should be invalid" do
+				addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
 				addresses.each do |invalid_address|
 					@user.email = invalid_address
 					expect(@user).not_to be_valid
@@ -92,3 +91,5 @@ describe User do
 				end
 			end
 		end
+	end
+	
